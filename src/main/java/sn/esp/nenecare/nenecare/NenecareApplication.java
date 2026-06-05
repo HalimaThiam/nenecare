0462af30286208c9d95d818ec8629177c52d8fa8
Halima Thiam <halimadiawo.thiam@esp.sn>
2026-06-05 18:35:00 +0000

    feat(crypto): setup Spring Boot + Bouncy Castle + JWT dependencies

---
+++ src/main/java/sn/esp/nenecare/nenecare/NenecareApplication.java
@@ -0,0 +1,13 @@
+package sn.esp.nenecare.nenecare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NenecareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NenecareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sn/esp/nenecare/nenecare/NenecareApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.esp.nenecare.nenecare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NenecareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
